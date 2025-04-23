24980d9ddb9008bc7cfa3aa29aff7c12868258cf
medhivrroom <sammedhi2@hotmail.fr>
2025-04-23 11:21:53 +0200

    First full implementation of the fetch command, now you can grab a file on a target directory you should specify in the command the server addr and the filepath. It will grab it, put it in the cache folder of the application and save it in a directory with the name of the file. Then it will try to open it for edition

--- cmd/fetch.go
+++ cmd/fetch.go
@@ -3,6 +3,11 @@ package cmd
 import (
 	"fmt"
 	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -13,11 +18,81 @@ import (
 var serverAddr string
 var filePath string
 var username string = "ftp"
+var password string = "!MonMotDePasse5"
 
 func init() {
 	fetchCmd.Flags().StringVar(&serverAddr, "addr", "", "The of ther server that hold the file")
 	fetchCmd.Flags().StringVar(&filePath, "file_path", "", "The path where the file to fetch is located inside the server")
 	rootCmd.AddCommand(fetchCmd)
+}
+
+func login(servAddr string, username string, password string) (*ftp.ServerConn, error) {
+	c, err := ftp.Dial(servAddr, ftp.DialWithTimeout(5*time.Second))
+
+	if err != nil {
+		return nil, fmt.Errorf("could not connect to the server %s; %v", servAddr, err)
+	}
+
+	err = c.Login(username, password)
+
+	if err != nil {
+		return nil, fmt.Errorf("could not login to %s as %s; %v", servAddr, username, err)
+	}
+
+	return c, nil
+}
+
+func fetchFile(c *ftp.ServerConn, filePath string, fileDest string) error {
+	file, err := c.Retr(filePath)
+
+	if err != nil {
+		return fmt.Errorf("could not retrieve the file '%s'; %v", filePath, err)
+	}
+
+	defer file.Close()
+
+	// Ceate the directory if it does not exist
+	dir := filepath.Dir(fileDest)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err = os.MkdirAll(dir, os.ModePerm)
+		if err != nil {
+			return fmt.Errorf("directory creation failed; %v", err)
+		}
+	}
+
+	// Create the file
+	dst, err := os.Create(fileDest)
+	if err != nil {
+		return fmt.Errorf("file creation failed; %v", err)
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, file)
+
+	if err != nil {
+		return fmt.Errorf("could not copy the file '%s' to '%s'; %v", filePath, fileDest, err)
+	}
+
+	return nil
+}
+
+func fileNameWithoutExt(fileName string) string {
+	return strings.TrimSuffix(filepath.Base(fileName), filepath.Ext(fileName))
+}
+
+func openFileWithDefaultProgram(filePath string) error {
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "start", "", filePath)
+	case "darwin":
+		cmd = exec.Command("open", filePath)
+	default: // linux, freebsd, etc.
+		cmd = exec.Command("xdg-open", filePath)
+	}
+
+	return cmd.Start()
 }
 
 var fetchCmd = &cobra.Command{
@@ -32,45 +107,31 @@ var fetchCmd = &cobra.Command{
 			return err
 		}
 
-		// Try to connect to the server
-		c, err := ftp.Dial(serverAddr, ftp.DialWithTimeout(5*time.Second))
+		c, err := login(serverAddr, username, password)
 
 		if err != nil {
-			return fmt.Errorf("could not connect to the server %s; %v", serverAddr, err)
-		}
-
-		// Try to login to the server
-		err = c.Login(username, "!MonMotDePasse5")
-
-		if err != nil {
-			return fmt.Errorf("could not login to %s as %s; %v", serverAddr, username, err)
+			return err
 		}
 
 		defer c.Quit()
 
-		fmt.Printf("successfully logged as %s\n", username)
-
-		// Try to retrieve the file
-		fmt.Printf("try to retrieve the file '%s'\n", filePath)
-		file, err := c.Retr(filePath)
-
-		if err != nil {
-			return fmt.Errorf("could not retrieve the file '%s'; %v", filePath, err)
-		}
-
-		defer file.Close()
-		fmt.Printf("successfully retrieved \n")
-
 		// Try to read the file
-		fmt.Printf("try to read the file %s\n", filePath)
-		buf, err := io.ReadAll(file)
+		cacheDir, _ := os.UserCacheDir()
+		outputPath := filepath.Join(cacheDir, "scriptmanager", fileNameWithoutExt(filePath), filepath.Base(filePath))
+		err = fetchFile(c, filePath, outputPath)
 
 		if err != nil {
-			fmt.Printf("Could not read the file %s\n", filePath)
-			return err
+			return fmt.Errorf("could not fetch the file '%s'; %v", filePath, err)
 		}
 
-		fmt.Printf("file content : %s", buf)
+		fmt.Printf("File '%s' fetched successfully to '%s'\n", filePath, outputPath)
+
+		err = openFileWithDefaultProgram(outputPath)
+
+		if err != nil {
+			return fmt.Errorf("could not open the file '%s'; %v", outputPath, err)
+		}
+
 		return nil
 	},
 }
